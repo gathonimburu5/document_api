2f7307d02e06562814c32c92e724320ca24a3d0b
Mburu Gathoni <paul.mburu@ngenx.io>
2026-08-12 15:05:30 +0300

    update from uv run to using gunicorn and other changes within the system

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ set -e
 
 python manage.py migrate --no-input
 
-exec python manage.py runserver 0.0.0.0:8081
+# exec python manage.py runserver 0.0.0.0:8081
+
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8081
